8362803e4becbc8058876073b5fc7983aaa6c25c
Leone <leon.fajardoperalta@elev.ga.ntig.se>
2024-10-01 13:27:11 +0200

    lhjbkjhvkjhv

---
+++ Slagsmål/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
+
+int hHp = 100;
+int vHp = 100;
+
+string He = "dog";
+string Vi = "cat";
+
+Console.WriteLine (Vi);
+Console.WriteLine (He);
+
+if ("no time")
+
+Console.ReadLine("He" + "Vi");
