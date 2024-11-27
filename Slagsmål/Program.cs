5eb6b227d788bf87c1648e7c8bbded38039d27b6
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-27 14:45:15 +0100

    slagsmål är klar

--- Slagsmål/Program.cs
+++ Slagsmål/Program.cs
@@ -1,15 +1,44 @@
-﻿using System.Runtime.CompilerServices;
-using System.Threading.Tasks.Dataflow;
+﻿
+int op = 100;
+int dop = 100;
 
-int hHp = 100;
-int vHp = 100;
+// hp på båda gubbarna jag har planerat 
 
-string He = "dog";
-string Vi = "cat";
 
-Console.WriteLine (Vi);
-Console.WriteLine (He);
+// ska spelas så länge deras inte är 0
+// hade tänkt att döda båda om en dör 
+while (dop != 0 && op != 0 )
+{
 
-if ("no time")
 
-Console.ReadLine("He" + "Vi");
+    int num = Random.Shared.Next(1, 11);
+    dop = dop - num;
+
+    num = Random.Shared.Next(1, 11);
+    op = op - num;
+
+   Console.WriteLine(dop);
+   
+   Console.WriteLine(op);
+
+   if (op <= 0 && op < dop)
+   {
+    op = 0;
+    Console.WriteLine("opp förlora");
+   }
+   
+   if (dop <= 0 && dop < op)
+   {
+    Console.WriteLine("dopp förlora");
+    dop = 0;
+   }
+   
+
+   Console.ReadLine();
+
+}
+
+   if (op == 0 && op == 0)
+   {
+    Console.WriteLine("oavgjort");
+   }
